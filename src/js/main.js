39b3160c48e4cca9c9b8f45edda17d9362810db7
Ragim Guseynov <rmx.880@gmail.com>
2018-08-04 16:33:58 +0400

    form download input button photo

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	
 	$(".left-panel").customScrollbar({preventDefaultScroll: true});
 
+	// Upload file. Кнопка загрузки фотографии
+	$('.file__upload-input').change(function() {
+  		var label = $(this).parent().find('label');
+  		if($(this).val() === '') {
+     		label.text('No File');
+  		} else {
+     	label.text($(this).val().replace('C:\\fakepath\\',''));
+  		}
+	});
+
 });
