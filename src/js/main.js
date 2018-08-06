b517a1bc0afb7a6616f2988eb3d3aad1f9bbacd4
Ragim Guseynov <rmx.880@gmail.com>
2018-08-06 10:37:55 +0400

    form download input button photo

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 	$(".left-panel").customScrollbar({preventDefaultScroll: true});
 
 	// Upload file. Кнопка загрузки фотографии
-	$('.file__upload-input').change(function() {
-  		var label = $(this).parent().find('label');
-  		if($(this).val() === '') {
-     		label.text('No File');
-  		} else {
-     	label.text($(this).val().replace('C:\\fakepath\\',''));
-  		}
-	});
+  $('.inputfile').change(function() {
+      var label = $(this).parent().find('span');
+      var files = $(this)[0].files;
+      if(files.length >= 2){
+        label.text("Вы выбрали " + this.files.length + " файла");
+      }  else {
+        label.text($(this).val().replace('C:\\fakepath\\',''));
+      }  
+  });
 
 });
